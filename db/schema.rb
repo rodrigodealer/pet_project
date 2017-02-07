d827c5a6de6eaee508dcdea1fb542ab5a6536150
Rodrigo Oliveira <rol@tradeshift.com>
2017-02-07 22:05:13 +0100

    #20 - Product tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121173442) do
+ActiveRecord::Schema.define(version: 20170207210049) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20170121173442) do
   end
 
   create_table "product_options", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_tags", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
---
+++ spec/factories/product_tags.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :product_tag do
+    name "MyString"
+  end
+end
