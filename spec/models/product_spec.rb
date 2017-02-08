f41738d3420f128975cf08f1564627669cfb0709
Rodrigo Oliveira <rol@tradeshift.com>
2017-02-08 18:40:59 +0100

    Fix specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:price) }
-  it { should validate_presence_of(:available) }
   it { should belong_to(:brand) }
 
   let(:product) { Product.new(name: 'Frontline', price: 20.0, available: true, properties: {'Color' => 'Black'}) }
