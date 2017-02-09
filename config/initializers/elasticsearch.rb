bf31159cd4854b0dec7a4ac725f2900249883199
Rodrigo Oliveira <rol@tradeshift.com>
2017-02-09 14:41:25 +0100

    Search stuff

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,10 +1,6 @@
-config = {
-  host: "http://localhost:9200/",
-  transport_options: {
-    request: { timeout: 5 }
-  }
-}
 env = 'development'
-env = ENV['RAILS_ENV'] if ENV['RAILS_ENV']
+if !ENV['RAILS_ENV'].nil?
+  env = ENV['RAILS_ENV']
+end
 config = YAML.load_file(Rails.root.join('config/elasticsearch.yml'))[env]
 Elasticsearch::Model.client = Elasticsearch::Client.new(config)
