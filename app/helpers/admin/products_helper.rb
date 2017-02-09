18e170162ca456b4cfe410e89e00efbec1a8249b
Rodrigo Oliveira <rol@tradeshift.com>
2017-02-09 10:53:59 +0100

    #20 - Better view on products 😎

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -2,4 +2,8 @@ module Admin::ProductsHelper
   def available?(product)
     product.available ? 'Sim' : 'Não'
   end
+
+  def availability_class(product)
+    product.available ? 'success' : 'danger'
+  end
 end
