ed4eb188dc613d535f5c443e9e6c3988d1385806
Rodrigo Oliveira <rol@tradeshift.com>
2017-02-09 15:58:58 +0100

    Stuff

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,6 +2,6 @@ env = 'development'
 if !ENV['RAILS_ENV'].nil?
   env = ENV['RAILS_ENV']
 end
-
-config = YAML.load(ERB.new(File.read("#{Rails.root}/config/elasticsearch.yml")).result)[env]
-Elasticsearch::Model.client = Elasticsearch::Client.new(config)
+file = File.read("#{Rails.root}/config/elasticsearch.yml")
+config = YAML.load(ERB.new(file).result)[env]
+Elasticsearch::Model.client = Elasticsearch::Client.new(host: config['host'])
