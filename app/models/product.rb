bcc33e221a699a9b6e6ce764a4bfdc417123cd75
Rodrigo Oliveira <rol@tradeshift.com>
2017-02-09 12:28:26 +0100

    Cleanup

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+  index_name([Rails.env,base_class.to_s.pluralize.underscore].join('_'))
   
   validates :name, :price, presence: true
   belongs_to :brand
