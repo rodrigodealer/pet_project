27c1d80acfe53af765b221e8becae116b4d59370
Rodrigo Oliveira <rol@tradeshift.com>
2017-02-09 12:06:48 +0100

    Cleanup

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -4,10 +4,5 @@ config = {
     request: { timeout: 5 }
   }
 }
-# env = ENV['RAILS_ENV']
-# if ENV['RAILS_ENV'].nil?
-#   env = 'development'
-# end
-# config = YAML.load_file("config/elasticsearch.yml")[ENV['RAILS_ENV']] if File.exists?("config/elasticsearch.yml")
 config = YAML.load_file(Rails.root.join('config/elasticsearch.yml'))[ENV['RAILS_ENV']]
 Elasticsearch::Model.client = Elasticsearch::Client.new(config)
