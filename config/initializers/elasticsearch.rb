319d2388c5c376786fadda4e9ccc861293d3a36c
Rodrigo Oliveira <rol@tradeshift.com>
2017-02-09 11:30:58 +0100

    #16 - Elasticsearch fix

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -4,8 +4,10 @@ config = {
     request: { timeout: 5 }
   }
 }
-
-
-config = YAML.load_file("config/elasticsearch.yml")[Rails.env] if File.exists?("config/elasticsearch.yml")
+env = ENV['RAILS_ENV']
+if ENV['RAILS_ENV'].nil?
+  env = 'development'
+end
+config = YAML.load_file("config/elasticsearch.yml")[ENV['RAILS_ENV']] if File.exists?("config/elasticsearch.yml")
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(config)
