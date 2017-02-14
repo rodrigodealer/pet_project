1b964a0c393cf29e2cde2fa341ca88a28d26197e
Rodrigo Oliveira <rol@tradeshift.com>
2017-02-14 21:24:51 +0100

    Product page

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -7,4 +7,17 @@ module ProductsHelper
   def qty_items
     (1..10).map {|i| [i, i]}
   end
+
+  def label_shuffle(tags)
+    options = ['default', 'primary', 'success', 'info', 'warning', 'danger']
+    tags.map do |tag|
+      item = options.sample
+      options.delete(item)
+      {name: tag, label: item}
+    end
+  end
+
+  def properties_comma_separated(properties, property)
+    properties.first[property].map{|t| t.first }.join(', ')
+  end
 end
--- spec/helpers/products_helper_spec.rb
+++ spec/helpers/products_helper_spec.rb
@@ -12,6 +12,8 @@ require 'rails_helper'
 # end
 RSpec.describe ProductsHelper, type: :helper do
   let(:items) { [['G', 20], ['P', 30], ['M', 10]] }
+  let(:tags)  { ['gato', 'cachorro', 'bode'] }
+  let(:properties) { ['Tamanho' => [['G', '20'], ['P', '30'], ['M', '10']],'Cor' => [['Preto', '10']],'Tipo' => [['Gato', '40']]] }
 
   it 'label_for_properties' do
     expect(helper.label_for_properties(items).first).to eq(['G - R$ 20', 'G'])
@@ -21,5 +23,18 @@ RSpec.describe ProductsHelper, type: :helper do
 
   it 'qty_items' do
     expect(helper.qty_items).to eq([[1,1], [2,2],[3,3],[4,4],[5,5],[6,6],[7,7], [8,8], [9,9], [10,10]])
+  end
+
+  it 'label_shuffle' do
+    labels = helper.label_shuffle(tags)
+    expect(labels.size).to eq(3)
+  end
+
+  it 'gets the property as a comma separated list' do
+    expect(helper.properties_comma_separated(properties,'Tamanho')).to eq('G, P, M')
+  end
+
+  it 'gets the property as just one item' do
+    expect(helper.properties_comma_separated(properties,'Cor')).to eq('Preto')
   end
 end
