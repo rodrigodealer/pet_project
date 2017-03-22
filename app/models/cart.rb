f2c36251032d8aa67fe4cfe39f652a7bb8fe30d0
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2017-03-22 11:52:28 -0300

    Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -13,8 +13,15 @@ class Cart
     with_items(JSON.parse(items))
   end
 
-  def with_items(items)
-    @items << items
+  def with_items(items_to_add)
+    has_copy = false
+    @items.flatten.map do |item|
+      item['qty'] = add_sum(item, items_to_add) if is_in_cart?(item, items_to_add)
+      has_copy = true if is_in_cart?(item, items_to_add)
+      item
+    end
+
+    @items << items_to_add.first if has_copy == false
     self
   end
 
@@ -30,5 +37,14 @@ class Cart
   def save
     Cart.redis.set(user_id, self.items.to_json)
     self.items.to_json
+  end
+
+  private
+  def add_sum(item_in_cart, item_to_add)
+    (item_in_cart['qty'].to_i + item_to_add.first['qty'].to_i).to_s
+  end
+
+  def is_in_cart?(item_in_cart, item_to_add)
+    item_in_cart['property'] == item_to_add.first['property'] && item_in_cart['product_id'] == item_to_add.first['product_id']
   end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -6,6 +6,35 @@ RSpec.describe Cart, type: :model do
 
   let(:redis) { MockRedis.new }
 
+  describe 'duplication protection for cart' do
+    let(:obj1) { [{'product_id' => '1', 'qty' => '1', 'property' => { 'Tamanho' => 'G', 'Cor' => 'Preto', 'Tipo' => 'Gato' }}] }
+    let(:obj2) { [{'product_id' => '1', 'qty' => '3', 'property' => { 'Tamanho' => 'G', 'Cor' => 'Preto', 'Tipo' => 'Gato' }}] }
+    let(:obj3) { [{'product_id' => '1', 'qty' => '3', 'property' => { 'Tamanho' => 'G', 'Cor' => 'Preto', 'Tipo' => 'Cachorro' }}] }
+
+    before do
+      allow(Redis).to receive(:new) { redis }
+    end
+
+    it 'has an item and tries to add the same' do
+      result1 = subject.with_items(obj1)
+      result2 = subject.with_items(obj2)
+
+      expect(result1.items.size).to be_eql(1)
+      expect(result2.items.size).to be_eql(1)
+      expect(result2.items.first['qty']).to be_eql('4')
+    end
+
+    it 'has two items (equal items) and add a new one' do
+      subject.with_items(obj1)
+      subject.with_items(obj2)
+      result3 = subject.with_items(obj3)
+
+      expect(result3.items.size).to be_eql(2)
+      expect(result3.items.first['qty']).to be_eql('4')
+      expect(result3.items.last['qty']).to be_eql('3')
+    end
+  end
+
   it 'has user_id eq to 1' do
     expect(subject.user_id).to eq(1)
   end
@@ -16,12 +45,12 @@ RSpec.describe Cart, type: :model do
 
   it 'adds items based on json' do
     json = "[{\"product_id\":\"1\"}]"
-    expect(subject.with_json_items(json).items).to eq([[{'product_id' => '1'}]])
+    expect(subject.with_json_items(json).items).to eq([{'product_id' => '1'}])
   end
 
   it 'adds items based on object' do
     obj = [{'product_id' => '1'}]
-    expect(subject.with_items(obj).items).to eq([[{'product_id' => '1'}]])
+    expect(subject.with_items(obj).items).to eq([{'product_id' => '1'}])
   end
 
   it 'gets the unknown cart' do
@@ -43,8 +72,8 @@ RSpec.describe Cart, type: :model do
     obj = [{'product_id' => '1'}]
 
     result = subject.with_items(obj).save
-    
-    expect(result).to be_eql([obj].to_json)
-    expect(redis.get('1')).to eq("[[{\"product_id\":\"1\"}]]")
+
+    expect(result).to be_eql(obj.to_json)
+    expect(redis.get('1')).to eq("[{\"product_id\":\"1\"}]")
   end
 end
